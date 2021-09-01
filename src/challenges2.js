3eaebf1eceeaebac93a5f51e960a80a9146be430
leandro <leandrochagask@gmail.com>
2021-08-31 23:32:49 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, inName) {
+  let message = [];
+  let orderedTech = tech.sort();
+  let finalList = [];
+  if (orderedTech.length !== 0) {
+    for (let index in orderedTech) {
+      let list = {
+        name: inName,
+        tech: orderedTech[index],
+      };
+      finalList.push(list);
+      message = finalList;
+    }
+  } else {
+    message = 'Vazio!';
+  }
+  return message;
 }
 
 // Desafio 11
@@ -11,8 +26,6 @@ function generatePhoneNumber() {
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
-
-  
 }
 
 // Desafio 13
